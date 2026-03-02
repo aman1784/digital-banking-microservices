67cee60f493487bc5be366f47868ce4169c31fc9
AMAN KUMAR SETH <amanthecoder04@gmail.com>
2026-03-02 22:12:32 +0530

    JwtAuthenticationFilter.java commented JwtUtil.java commented

--- account-service/accountservice/src/main/java/com/bank/accountservice/security/JwtUtil.java
+++ account-service/accountservice/src/main/java/com/bank/accountservice/security/JwtUtil.java
@@ -1,36 +1,36 @@
-package com.bank.accountservice.security;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.List;
-
-@Component
-public class JwtUtil {
-
-    private final Key key;
-
-    public JwtUtil(@Value("${jwt.secret}") String secret) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes());
-    }
-
-    public Claims extractClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
-    }
-
-    public String extractUsername(String token) {
-        return extractClaims(token).getSubject();
-    }
-
-    public List<String> extractRoles(String token) {
-        return extractClaims(token).get("roles", List.class);
-    }
-}
+//package com.bank.accountservice.security;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.security.Keys;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import javax.crypto.SecretKey;
+//import java.util.List;
+//
+//@Component
+//public class JwtUtil {
+//
+//    private final SecretKey key;
+//
+//    public JwtUtil(@Value("${jwt.secret}") String secret) {
+//        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+//    }
+//
+//    public Claims extractClaims(String token) {
+//        return Jwts.parser()
+//                .verifyWith(key)
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload();
+//    }
+//
+//    public String extractUsername(String token) {
+//        return extractClaims(token).getSubject();
+//    }
+//
+//    public List<String> extractRoles(String token) {
+//        return extractClaims(token).get("roles", List.class);
+//    }
+//}
